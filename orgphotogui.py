170bf3a862018272d0ad111babaecce1b00718de
WZS <welwel2@yahoo.com>
2017-08-30 15:21:31 -0600

    initial project version

---
+++ orgphotogui.py
@@ -0,0 +1,176 @@
+import sys
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+from socket import *                         # including socket.error 
+from tkinter.scrolledtext import ScrolledText
+from launchmodes import PortableLauncher
+import time
+
+
+class OrgPhotosGUI(Frame):
+    rev = '1.0'
+    msg0 = 'Welcome to Photo Organizer rev %s.\nSelect your input and output folders before proceeding.\n'%rev
+    msg1 = 'Need to select a folder before proceeding..\n'
+    msg2 = 'proceeding with your request..\n'
+    msg3 = 'Photo Organizer rev %s' %rev
+    source_folder = 'NA'
+    destination_folder = 'NA'
+    
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.pack(expand=YES, fill=BOTH)
+        self.starttime = time.time()
+        self.makewidgets()
+        self.config(bg='cyan')
+        self._updatetext(self.msg0)
+        self.onTimer()
+        
+    def _startSocket(self):
+        myport = 50008
+        self._updatetext('starting socket at port number %d\n'%myport)
+        self.sockobj = socket(AF_INET, SOCK_STREAM)       # GUI is server, script is client
+        self.sockobj.bind(('', myport))                   # config server before client
+        self.sockobj.listen(5)
+        
+    def onTimer(self):
+        secsSinceEpoch = time.time()
+        elapsedtime = secsSinceEpoch - self.starttime
+        elapsedsec = elapsedtime % 60      # extract seconds
+        elapsedminh = elapsedtime // 60
+        elapsedhrs = elapsedminh  // 60    # extract hours
+        elapsedmin = elapsedminh % 60      # extract minutes
+
+        # update status Bar
+        self.log_st1.config(text=time.ctime(secsSinceEpoch))
+        self.log_st3.config(text='session time %d:%d:%d'%(elapsedhrs, elapsedmin, elapsedsec))
+        
+        self.after(1000 // 1, self.onTimer)  # run N times per second
+        
+    def checkdata(self):
+        # check data from spawned non gui program
+        try:
+            message = self.conn.recv(1024)            # don't block for input
+            if(message):
+                self._updatetext(message)
+        except error:                            # raises socket.error if not ready
+            pass
+#            self._updatetext('no data..\n')                     
+        self.after(500, self.checkdata)              # check once per second
+        
+    def get_folder(self, src_dest):
+        folder_path = askdirectory()
+        if not folder_path: return
+        self._updatetext('set entry folder path %s\n' %folder_path)
+        if src_dest == 'src':
+            self.source_l.insert(0,'%s'%folder_path)
+            self.source_l.config(bg='green', fg='white')
+        else:
+            self.dest_l.insert(0,'%s'%folder_path)
+            self.dest_l.config(bg='green', fg='white')
+    
+    def call_organize(self, dest=False):
+        if dest == False:
+            self.destination_folder = ''
+        if not self._validate_folder(dest=dest): return
+        self._updatetext(self.msg2)
+        self._startSocket()
+        self._updatetext('Spawn non GUI script\n')
+        cmd = 'orgpics.pyw %s %s -g'%(self.source_folder, self.destination_folder)
+        PortableLauncher('Organize', cmd)()  # spawn non-GUI script 
+
+        self._updatetext('accepting\n')
+        self.conn, self.addr = self.sockobj.accept()                # wait for client to connect
+        self._updatetext('accepted\n')
+        self.conn.setblocking(False)                           # use nonblocking socket (False=0)
+
+        self.checkdata()
+            
+    def _validate_folder(self, src=True, dest=True):
+        folder_list = [('source', self.source_l), ('desinantion', self.dest_l)]
+        if dest == False:
+            folder_list.pop(1)
+        for name, entry in folder_list:
+            folder = entry.get()
+            if not os.path.isdir(folder):
+                self._updatetext('Please enter or select a valid %s folder name\n'%name)
+                return False
+            else:
+                if name == 'source':
+                    self.source_folder = folder
+                else:
+                    self.destination_folder = folder
+                self._updatetext('%s folder is %s\n'%(name, folder))
+        if self.source_folder == self.destination_folder:
+            self._updatetext('Source and destination folders cannot be the same\n')
+            return False
+        return True    
+        
+    def _updatetext(self, msg):
+        self.log_t.configure(state='normal')
+        self.log_t.insert('end', msg)
+        self.log_t.see('end')
+        self.log_t.configure(state='disabled')
+        self.update()
+        
+    def makewidgets(self):
+        
+        # Build botton buttons first to collapse last!
+        b_frame = Frame(self, bg='cyan')
+        go_button = Button(b_frame, text='Organize', width=20, fg='white', bg='green',
+                           command=lambda:self.call_organize())
+        go_button.pack(side=LEFT)
+        c_button = Button(b_frame, text='Clean Folder', width=20, fg='white', bg='green',
+                           command=lambda:self.call_organize(dest=False))
+        c_button.pack(side=LEFT)
+        mid_label = Label(b_frame, text=self.msg3, bg='orange', font=('courier', 14))
+        mid_label.pack(side=LEFT, expand=YES, fill=X)
+        quit_button = Button(b_frame, text='Quit', width=20, fg='white', bg='red',
+                             command=root.quit)
+        quit_button.pack(side=RIGHT)
+        b_frame.pack(side=BOTTOM, fill=X)
+
+        # Build folders selectors
+        source_row = Frame(self)
+        source_label = Label(source_row, text="Source Folder:", width=20, justify=LEFT, bg='orange')
+        source_label.pack(side=LEFT, fill=X)
+        source_button = Button(source_row, text="select",
+                               bg='green', width=15, fg='white',
+                               command=lambda: self.get_folder('src'))
+        source_button.pack(side=RIGHT, padx=15)
+        self.source_l = Entry(source_row, relief="ridge", justify=LEFT)
+        self.source_l.pack(side= LEFT, fill=X, expand=YES)
+        source_row.pack(side=TOP, fill=X)
+
+        dest_row = Frame(self)
+        dest_label = Label(dest_row, text="Destination Folder:", width=20, justify=LEFT, bg='orange')
+        dest_label.pack(side=LEFT, fill=X)
+        dest_button = Button(dest_row, text="select",
+                             bg='green', width=15, fg='white',
+                             command=lambda: self.get_folder('dest'))
+        dest_button.pack(side=RIGHT, padx=15)
+        self.dest_l = Entry(dest_row, relief="ridge", justify=LEFT)
+        self.dest_l.pack(side= LEFT, fill=X, expand=YES)
+        dest_row.pack(side=TOP, fill=X)
+        
+        # Build Message for logging
+        self.log_t = ScrolledText(self, relief="ridge", bg='white', state='disabled')
+        self.log_t.pack(side=TOP, fill=BOTH, expand=Y)
+        
+        # Build status bar for logging
+        b_frame = Frame(self, bg='red')
+        self.log_st1 = Label(b_frame, relief="ridge", bg='purple', width=25, fg='white')
+        self.log_st1.pack(side=LEFT)
+        self.log_st2 = Label(b_frame, relief="sunken", bg='blue', width=50)
+        self.log_st2.pack(side=LEFT, fill=X,expand=Y)
+        self.log_st3 = Label(b_frame, relief="ridge", bg='purple', width=25, fg='white')
+        self.log_st3.pack(side=LEFT)
+        b_frame.pack(side=BOTTOM, fill=X)
+        
+
+if __name__ == '__main__':
+    root = Tk()
+    myorgphoto = OrgPhotosGUI(root)
+    myorgphoto.master.title("My Org Photo")
+    root.mainloop()
+
---
+++ orgpics.pyw
@@ -0,0 +1,171 @@
+import os
+import exifread
+import hashlib
+from PIL import Image
+import time
+from multiprocessing import *
+from socket_stream_redirect0 import redirectOut        # connect my sys.stdout to socket
+
+class OrgPics:
+    organize = True
+    fhashs = []
+    def __init__(self, input_f, output_f='', redirect = False):
+        self.starttime = time.time()
+        self.input_f = input_f
+        self.output_f = output_f
+        self.redirect = redirect
+        if output_f == '':
+            self.organize = False
+            self.duplicates = os.path.join(input_f, 'duplicates')
+            self.new_location = self.duplicates
+        else:
+            self.duplicates = os.path.join(output_f, 'duplicates')
+            self.dateless = os.path.join(output_f, 'dateless')
+                
+        if self.organize and not os.path.exists(output_f):
+            # path does not exist, create a new folder
+            os.mkdir(output_f)
+        self.run()
+        
+    def run(self):
+        rmlist = []
+        file_counter = 0
+        flist = []
+        for (dirname, subshere, fileshere) in os.walk(self.input_f):
+            for fname in fileshere:
+                if fname[-3:] in ('jpg', 'JPG'):
+                    file_counter += 1              # increment counter
+                    flist.append(os.path.join(dirname, fname))
+            if not os.path.getsize(dirname):
+                rmlist.append(dirname)
+        self.rm_empty_folders(rmlist)
+        self.callmulti_process2(flist)
+        print('%d files processed in %d seconds'%(file_counter, time.time()- self.starttime))            
+        
+    def rm_empty_folders(self, rmlist):
+        # remove empty folders
+        map(os.rmdir, rmlist)
+       # for folder in rmlist:
+       #     print('removing empty folder %s' %folder)
+       #     try:
+       #         os.rmdir(folder)
+       #     except:
+       #         print('failed to remove folder %s'%folder)
+       #         pass
+        
+    def callmulti_process2(self, flist):
+        MAX_POOL = cpu_count() * 2
+        result = []
+        files = len(flist)
+        if files < MAX_POOL:
+            MAX_POOL = files
+        P = Pool(MAX_POOL)
+        P.map(self.processfile, flist)
+        
+    #def callmulti_process(self, flist):
+    #    lock = Lock()
+    #    files = len(flist)
+    #    while len(flist):
+    #        print ('number of files to process %d' %len(flist))
+    #        if len(flist) > 10:
+    #            processes = 10
+    #        else:
+    #            processes = len(flist)
+    #
+    #        for i in range(processes):
+    #            print ('processes %d %d file %s' %(processes, i, flist[0]))
+    #            Process(target=self.processfile, args=(('run process %d' % i),
+    #                                                    flist[0], lock)).start()
+    #            flist.pop(0)
+        
+    def processfile(self, file):
+        msg = 'pid:%s, processing file:%s'
+        self.file_path = file
+       # if self.redirect:
+       #     redirectOut()     # GUI must be started first as is
+        print(msg % ( os.getpid(), self.file_path))
+        self.checkDuplicates()
+
+        if self.organize:
+            # extract metadata from file
+            self.extractOriginalDate()
+
+            # move file to new location
+            self.moveFile()
+        
+    def extractOriginalDate(self):
+        # open image file for reading (binary mode)
+        fh = open(self.file_path, 'rb')
+        
+        # Return Exif tags
+        tags = exifread.process_file(fh)
+        # print(tags)
+        try:
+            dto = tags['EXIF DateTimeOriginal']
+        except KeyError:
+            try:
+                dto = tags['Image DateTime']
+            except KeyError:
+                print('Photo has no date found')
+                self.new_location = self.dateless
+                return
+                
+        # extract original date from metadata
+        year = dto.__str__()[:4]
+        month = dto.__str__()[5:7]
+        day = dto.__str__()[8:10]
+        
+        self.new_location = os.path.join(self.output_f, '%s-%s-%s' %(year, month, day))
+        print ("Photo Original Date: %s-%s-%s\n" %(year, month, day))
+    
+    def checkDuplicates(self):
+        fhash = self.hashi()
+        
+        #check to see if we have the file already
+        if fhash in self.fhashs:
+            # we have a possible file duplicate since the dto exists already
+            # move to duplicates folder
+            self.new_location = self.duplicates
+        #    print ("Photo is a duplicate\n")
+            self.moveFile()
+            return
+        else:
+            self.fhashs.append(fhash)
+        
+    def moveFile(self):
+        folder = self.new_location
+        if not os.path.exists(folder):
+            # path does not exist, create a new folder
+            os.mkdir(folder)
+        npl = os.path.join(folder, self.fname)
+        
+        if os.path.exists(npl):
+            print('Photo exists in new folder, skipping..')
+            return
+        
+        os.rename(self.file_path, npl)
+        
+    def hashi(self):
+        tf = Image.open(self.file_path)
+        return hashlib.md5(tf.tobytes()).hexdigest()
+     
+if __name__ == '__main__':
+    import sys
+    args = len(sys.argv)
+    redirect = False
+    default_path = r'C:\Users\220554\orgphotos'
+    print('sys.argv ', sys.argv, 'args ', args)
+    
+    if '-g' in sys.argv[-1]:                          # link to gui only if requested
+        print('redirecting output to gui ', sys.argv)
+        redirectOut()                                # GUI must be started first as is
+        redirect = True
+        sys.argv.pop(-1)
+        
+    if len(sys.argv) > 2:
+        op = OrgPics(input_f=sys.argv[1], output_f=sys.argv[2], redirect=redirect)
+    elif len(sys.argv) == 2:
+        op = OrgPics(input_f=sys.argv[1], redirect=redirect)
+    else:
+        op = OrgPics(input_f=default_path, redirect=redirect)
+        
